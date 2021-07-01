a40d30e0fa525773ffd654cb4bd4db2916e62c83
sol1105 <martin.schupfner@web.de>
2021-07-01 12:05:32 +0200

    Added new masks, grid file retrieval script and new test
    - Added new grid files with binary masks from the ATLAS github repository
    - Added bash script to retrieve ATLAS and CMIP6 reference grid files
    - Added test to check availability of all registered grid files

--- tests/test_roocs_grids.py
+++ tests/test_roocs_grids.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 
-from roocs_grids import get_grid_file
+from roocs_grids import get_grid_file, grid_dict
 
 
 def test_get_grid_file():
@@ -11,4 +11,14 @@ def test_get_grid_file():
     gf = get_grid_file("0pt25deg")
     assert os.path.isfile(gf)
     assert gf.endswith("roocs_grids/grids/cmip6_720x1440_scrip.20181001.nc")
+
+
+@pytest.mark.parametrize('grid_id,filename', [(key, grid_dict[key]) for key in grid_dict])
+def test_grid_avail(grid_id, filename):
+    with pytest.raises(Exception, match="Unknown grid id: rubbish"): 
+        get_grid_file("rubbish")
+    
+    gf = get_grid_file(grid_id)
+    assert os.path.isfile(gf)
+    assert gf.endswith("roocs_grids/grids/{}".format(filename))
 
